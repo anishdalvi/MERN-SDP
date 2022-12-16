21c5a21fcf3f16ce284048058bf51a33ba92e3bb
Anish Dalvi <anishdalvi2001@gmail.com>
2022-12-16 16:05:36 +0530

    Before Promise (Get and Post)

---
+++ Todo/server.js
@@ -0,0 +1,57 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const Todo = require("./models/todo-model")
+mongoose.set('strictQuery', true);
+mongoose.connect("mongodb://0.0.0.0:27017/newDB", (error) =>{
+    if (error) {
+        console.log("Error: ", error)
+    }
+    console.log("Connection Established Sucessfully")
+})
+const app = express()
+
+
+app.get("/greetings", (req, res) =>{
+    res.status(201).send("Hello World")
+})
+
+app.get("/todos", (req, res) =>{
+
+    res.status(200).json({
+        todo : {some : "task"}
+    })
+})
+
+app.post("/todos", (req, res) =>{
+
+    // const newTodo = req.body
+
+    // return Promise.resolve()
+    //     .then(() => {
+    //         if(!newTodo.title)
+    //             throw Error("Title from Throw")
+            
+    //         return Todo.create(newTodo)
+    //     })
+    //     .then(data =>{
+    //         res.status(201).json({
+    //             message : "Todo Created",
+    //             todo : data
+    //         })
+    //     })
+    //     .catch(error =>{
+    //         res.status(422).json({
+    //             error: error.toString()
+    //         })
+    //     })
+
+
+    res.status(201).json({
+        "message" : "Todo Created"
+    })
+})
+
+
+app.listen(8000, ()=> {
+    console.log("Server Running on Port 8000")
+})
