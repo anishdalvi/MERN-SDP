83c8d287994fcf3c7bc8190cdadd75080bb7d164
Anish Dalvi <anishdalvi2001@gmail.com>
2022-12-17 12:42:05 +0530

    useEffect dependency array

--- 2022-12-17 (Hooks, Redux)/react-tutorial/src/App.js
+++ 2022-12-17 (Hooks, Redux)/react-tutorial/src/App.js
@@ -1,53 +1,35 @@
 import './App.css';
-import{ useState, useRef } from 'react'
+import{ useEffect, useState } from 'react'
 
 
 function App() {
 
-  const [counter, setCounter] = useState(0)
-  const counterRef = useRef(200)
-  const inputRef = useRef()
+  const [ , dummy] = useState(true)
+  const [ count, setCount] = useState(true)
+  useEffect(() =>{
+    console.log("Use Effect Called with empty dependencies")
+    // With Empty dependency array this will print in console only 1 time
+    // Due to strict mode it console 2 times
+    // Removinh strict mode from index.js will print in console
+  }, [])
+  useEffect(() =>{
+    console.log("Use Effect Called without any dependencies")
+    // Without any dependency array this will print in console only 1 time
+    // Due to strict mode it console 2 times
+  })
 
-  // const counterData = useState(0)         // other way of writing Use State
-  // console.log(counterData)
 
-  function Increment(){
-    //setCounter(counter + 1)     // simple increment by 1
-    setCounter((count) =>count + 1)     // double increment using call back
-    setCounter((count) =>count + 1)
-  }
-  function Decrement(){
-    setCounter(counter - 1)
-  }
 
-  function incrementCountRef() {
-    counterRef.current = counterRef.current + 1
-    console.log("Counter Ref: ", counterRef.current)
-  }
-  function decrementCountRef() {
-    counterRef.current = counterRef.current - 1
-    console.log("Counter Ref: ", counterRef.current)
-  }
-
-  function inputButtonClicked(){
-    console.log(inputRef)
-  }
-  console.log("Counter Ref: ", counterRef.current)
   return (
     <div className="App">
-        <button onClick={incrementCountRef}>+</button>
-        <p>{counterRef.current}</p>
-        <button onClick={decrementCountRef}>-</button>
-        <br />
-        <MyCustomInputComponent inputRef = {inputRef} /> 
-        <button onClick={inputButtonClicked}>Click Button</button>
+        <button onClick={() => dummy((x) => (!x))}>
+          Click Me
+        </button>
     
     </div>
   );
 
-  function MyCustomInputComponent(props){
-    return <input ref={props.inputRef} />
-  }
+  
 }
 
 export default App;
