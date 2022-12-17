54688f7f3f6cfce492ba9426744ca79668d047a1
Anish Dalvi <anishdalvi2001@gmail.com>
2022-12-17 15:09:30 +0530

    memo and callback

--- 2022-12-17 (Hooks, Redux)/react-tutorial/src/App.js
+++ 2022-12-17 (Hooks, Redux)/react-tutorial/src/App.js
@@ -1,34 +1,52 @@
-import './App.css';
-import{ useEffect, useState, useMemo} from 'react'
+import React, {memo, useCallback, useEffect, useMemo, useRef, useState} from 'react'
 
-const agc = {
-  
-}
 function App() {
+  
+  const [count, setCount] = useState(0)
+  const [d, setD] = useState(false)
 
-  const [dummyValue , dummy] = useState(true)
-  const [ count, setCount] = useState(0)
-  
-  const memoizedCount = useMemo(() => count, [count])
-  
-  function updateCount(){
+  function updateCount() {
     setCount((x) => x+1)
   }
 
-  console.log( "Memoized Count: ", memoizedCount)
+  const updateCountArrow = () => {
+    console.log("Callback function called...")
+  }
 
-  return (
-    <div className="App">
-        
-    <button onClick={updateCount}>
-      Update Count
-    </button>
-    {count}
-    
-    </div>
-  );
+  console.log("Count: ", count)
 
-  
+
+  return <div style={{
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
+  }}>
+    <button onClick={updateCount}>Update Count</button>
+    <button onClick={() => setD((x) => !x)}>Render Callback Component</button>
+    <CouterPresentor count={count} customObject={{
+      "name": "swapnil"
+    }}/>
+    <UseCallbackExample updateCountArrow={updateCountArrow} />
+  </div>
 }
+
+const UseCallbackExample = memo((props) => {
+  console.log("Redndered becase of reference change of arrow function...")
+  return null
+})
+
+const CouterPresentor = memo((props) => {
+  const {count} = props
+
+  useEffect(() => {
+    console.log("CounterPresentor Rendered...")
+  })
+
+  return <div>
+    {count}
+  </div>
+}, (prev, next) => {
+  return prev === next
+})
 
 export default App;
