126b940c3fe3556c8d43f0ecd4b80219e8d9e62c
Anish Dalvi <anishdalvi2001@gmail.com>
2022-12-17 12:29:51 +0530

    props passing using useRef

--- 2022-12-17 (Hooks, Redux)/react-tutorial/src/App.js
+++ 2022-12-17 (Hooks, Redux)/react-tutorial/src/App.js
@@ -6,6 +6,7 @@ function App() {
 
   const [counter, setCounter] = useState(0)
   const counterRef = useRef(200)
+  const inputRef = useRef()
 
   // const counterData = useState(0)         // other way of writing Use State
   // console.log(counterData)
@@ -27,15 +28,26 @@ function App() {
     counterRef.current = counterRef.current - 1
     console.log("Counter Ref: ", counterRef.current)
   }
+
+  function inputButtonClicked(){
+    console.log(inputRef)
+  }
   console.log("Counter Ref: ", counterRef.current)
   return (
     <div className="App">
         <button onClick={incrementCountRef}>+</button>
         <p>{counterRef.current}</p>
         <button onClick={decrementCountRef}>-</button>
+        <br />
+        <MyCustomInputComponent inputRef = {inputRef} /> 
+        <button onClick={inputButtonClicked}>Click Button</button>
     
     </div>
   );
+
+  function MyCustomInputComponent(props){
+    return <input ref={props.inputRef} />
+  }
 }
 
 export default App;
