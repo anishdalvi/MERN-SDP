052de8a2882a44cdd2ea4afecf3c5f3525aea655
Anish Dalvi <anishdalvi2001@gmail.com>
2022-12-17 12:58:44 +0530

    useEffect as one and ref dependencies

--- 2022-12-17 (Hooks, Redux)/react-tutorial/src/App.js
+++ 2022-12-17 (Hooks, Redux)/react-tutorial/src/App.js
@@ -1,22 +1,30 @@
 import './App.css';
-import{ useEffect, useState } from 'react'
+import{ useEffect, useState, useRef } from 'react'
 
 
 function App() {
 
-  const [ , dummy] = useState(true)
-  const [ count, setCount] = useState(true)
+  const [dummyValue , dummy] = useState(true)
+  const [ count, setCount] = useState(0)
+  const counterRef = useRef(200)
   useEffect(() =>{
     console.log("Use Effect Called with empty dependencies")
     // With Empty dependency array this will print in console only 1 time
     // Due to strict mode it console 2 times
-    // Removinh strict mode from index.js will print in console
+    // Removing strict mode from index.js will print in console
   }, [])
   useEffect(() =>{
     console.log("Use Effect Called without any dependencies")
     // Without any dependency array this will print in console only 1 time
-    // Due to strict mode it console 2 times
+    
   })
+  useEffect(() =>{
+    console.log("Use Effect Called with one dependency (count)")
+  }, [count])
+
+  useEffect(() => {
+    console.log("Use Effect with ref dependency")
+  }, [counterRef.current])
 
 
 
@@ -25,6 +33,16 @@ function App() {
         <button onClick={() => dummy((x) => (!x))}>
           Click Me
         </button>
+        <button onClick={() => setCount((x) => (x + 1))}>
+          Update Count
+        </button>
+        <button onClick={() => {
+          counterRef.current = {}
+        }}>
+          Update Ref Count
+        </button>
+        <p>Dummy State : { dummyValue}, Count State : { count } </p>
+
     
     </div>
   );
