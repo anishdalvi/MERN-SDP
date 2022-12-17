c3bea239e69dcff14e9e872ea541cb01922757cb
Anish Dalvi <anishdalvi2001@gmail.com>
2022-12-17 14:17:11 +0530

    useMemon memoized count is not updated

--- 2022-12-17 (Hooks, Redux)/react-tutorial/src/App.js
+++ 2022-12-17 (Hooks, Redux)/react-tutorial/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import{ useEffect, useState, useRef } from 'react'
+import{ useEffect, useState, useMemo} from 'react'
 
 const agc = {
   
@@ -8,44 +8,22 @@ function App() {
 
   const [dummyValue , dummy] = useState(true)
   const [ count, setCount] = useState(0)
-  const counterRef = useRef(200)
-  useEffect(() =>{
-    console.log("Use Effect Called with empty dependencies")
-    // With Empty dependency array this will print in console only 1 time
-    // Due to strict mode it console 2 times
-    // Removing strict mode from index.js will print in console
-  }, [])
-  useEffect(() =>{
-    console.log("Use Effect Called without any dependencies")
-    // Without any dependency array this will print in console only 1 time
-    
-  })
-  useEffect(() =>{
-    console.log("Use Effect Called with one dependency (count)")
-  }, [count])
-
-  useEffect(() => {
-    console.log("Use Effect with ref dependency")
-  }, [counterRef.current])
-
-  function test(){
-    counterRef.current = agc
+  
+  const memoizedCount = useMemo(() => count, [])
+  
+  function updateCount(){
+    setCount((x) => x+1)
   }
 
+  console.log( "Memoized Count: ", memoizedCount)
 
   return (
     <div className="App">
-        <button onClick={() => dummy((x) => (!x))}>
-          Click Me
-        </button>
-        <button onClick={() => setCount((x) => (x + 1))}>
-          Update Count
-        </button>
-        <button onClick={test}>
-          Update Ref Count
-        </button>
-        <p>Dummy State : { dummyValue}, Count State : { count } </p>
-
+        
+    <button onClick={updateCount}>
+      Update Count
+    </button>
+    {count}
     
     </div>
   );
