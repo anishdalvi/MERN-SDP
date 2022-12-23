5465ae1c5444295eff1796a0f4c73064cde3d471
Anish Dalvi <anish.201914101@vcet.edu.in>
2022-12-23 16:20:21 +0530

    4 Test Cases of Register and Login

--- 2022-12-22,23 (MERN)/test/auth.test.js
+++ 2022-12-22,23 (MERN)/test/auth.test.js
@@ -26,7 +26,7 @@ describe('Authentication Test Cases', function () {
         it('Should register successfully', function(done){
             request(app).post('/api/auth/register')
             .send({
-                username: 'Test DB New',
+                username: 'test',
                 email: "test@email.com",
                 password:'1234'
             })
@@ -34,12 +34,57 @@ describe('Authentication Test Cases', function () {
                 console.log(err);
                 console.log(res.body);
                 expect(res.body.message).equal('Registration Successful')
-                expect(res.body.data.username).equal('Test DB New')
+                expect(res.body.data.username).equal('test')
                 expect(res.body.data.email).equal('test@email.com')
                 expect(res.body.data).not.have.property('password')
                 
             })
             done()
         })
+
+        it('Should register failed when email is not provided', (done) => {
+            request(app).post('/api/auth/register')
+            .send({
+                username:'test',
+                password:'1234'
+            })
+            .end((err, res) => {
+                expect(res.body.message).equal('Registration Failed')
+                expect(res.body.error).equal('Username, Email and Password not found')
+                done(err)
+                
+            })
+        })
+
+        describe('POST /login', function () 
+        {
+            it('should login successfully', function (done) {
+              request(app).post('/api/auth/login')
+                .send({
+                  email: 'test@email.com',
+                  password: '1234'
+                })
+                .end((err, res) => {
+                  expect(res.body.message).equal('Login Successful')
+                  expect(res.body).have.property('access_token')
+                  expect(res.body).not.have.property('password')
+                  done(err)
+                })
+            })
+        
+            it('should login fail when invalid password is provided', function (done) {
+              request(app).post('/api/auth/login')
+                .send({
+                  email: 'test@email.com',
+                  password: 'invalid'
+                })
+                .end((err, res) => {
+                  expect(res.body.message).equal('Login Failed')
+                  expect(res.body.error).equal('Invalid Password')
+                  done(err)
+                })
+            })
+        })
+
     })
 })
